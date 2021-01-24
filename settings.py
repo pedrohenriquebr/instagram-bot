7cc07f9ae096c1cddb807ec09457682600051ea3
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-01-24 17:55:43 -0300

    Fix HEADLESS check in settings

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ settings['password']  = os.getenv('PASSWORD')
 settings['post_link']  = os.getenv('LINK')
 settings['geckodriver']  = os.getenv('GECKODRIVER',False)
 settings['firefox_path']  = os.getenv('FIREFOX_PATH',False)
-settings['headless']  = os.getenv('HEADLESS',False)
+settings['headless']  = str(os.getenv('HEADLESS',False)).lower() == 'true'
 settings['custom_comment']  = os.getenv('CUSTOM_COMMENT',False)
 settings['min_random_delay']  = int(os.getenv('MIN_RANDOM_DELAY', 60))
 settings['max_random_delay']  = int(os.getenv('MAX_RANDOM_DELAY', 120))
