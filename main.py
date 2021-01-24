c24b3bccb05092444187b2f0acfb297c609efa0f
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-01-24 14:47:53 -0300

    Add settings.py

--- main.py
+++ main.py
@@ -4,35 +4,45 @@ from random import random
 from time import sleep
 from typing import Iterator, List
 
-from dotenv import load_dotenv
 from explicit import waiter, XPATH
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
-load_dotenv(override=True)
-
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-LINK = os.getenv('LINK')
-GECKODRIVER = os.getenv('GECKODRIVER', False)
-FIREFOX_PATH = os.getenv('FIREFOX_PATH', False)
-HEADLESS = os.getenv('HEADLESS',False)
-CUSTOM_COMMENT= os.getenv('CUSTOM_COMMENT', False)
-MIN_RANDOM_DELAY= int(os.getenv('MIN_RANDOM_DELAY', 60))
-MAX_RANDOM_DELAY= int(os.getenv('MAX_RANDOM_DELAY', 120))
+from settings import settings
 
 class Bot:
-    def __init__(self, username, password):
+    def __init__(self, username, password, post_link,
+                geckodriver=False,firefox_path=False,
+                headless=False,custom_comment=False,
+                min_random_delay=60,max_random_delay=120):
+        """
+        Starts a new bot instance.
+
+        :param username: account name used on Instagram.
+        :param password: password to login.
+        :param post_link: link to post on Instagram.
+        :param geckodriver: path to geckodriver executable.
+        :param headless: boolean for headless param for Firefox Options.
+        :param custom_element: a text to append to comment alongside mentions.
+        :param min_random_delay: minium integer seconds for random delay between each comments.
+        :param max_random_delay: maximum integer seconds for random delay between each comments.  
+        """
         self.username = username
         self.password = password
+        self.post_link = post_link
+        self.geckodriver = geckodriver
+        self.firefox_path = firefox_path
+        self.headless = headless
+        self.custom_comment = custom_comment
+        self.min_random_delay = min_random_delay
+        self.max_random_delay = max_random_delay
 
     def start(self):
         print('starting')
         self.options = Options()
-        self.options.headless = bool(HEADLESS)
-        if GECKODRIVER and FIREFOX_PATH:
+        self.options.headless = bool(self.headless)
+        if self.geckodriver and self.firefox_path:
             self.driver  = webdriver.Firefox(options=self.options, 
-            firefox_binary=FIREFOX_PATH, executable_path=GECKODRIVER)
+            firefox_binary=self.firefox_path, executable_path=self.geckodriver)
         else:
             self.driver  = webdriver.Firefox(options=self.options)
 
@@ -46,10 +56,10 @@ class Bot:
         print(len(self.followers_list))
         
         sleep(6)
-        self.open_post(LINK)
+        self.open_post(self.post_link)
         sleep(8)
         last_state = 0
-        state_path = f'{LINK}_state.txt'
+        state_path = f'{self.post_link}_state.txt'
         if os.path.exists(state_path):
             last_state = int(open(state_path,'r').readlines()[-1].strip())
         print('last index: ', last_state)
@@ -60,8 +70,8 @@ class Bot:
             comment_input.click()
             sleep(2)
             comment_input = self.driver.find_element_by_css_selector("textarea")
-            if CUSTOM_COMMENT:
-                comment_input.send_keys(f'{CUSTOM_COMMENT} @{a} @{b}')
+            if self.custom_comment:
+                comment_input.send_keys(f'{self.custom_comment} @{a} @{b}')
             else:
                 comment_input.send_keys(f'@{a} @{b}')
             sleep(6)
@@ -70,7 +80,7 @@ class Bot:
             last_state += 2
             last_state_file.write(f'{last_state}\n')
             last_state_file.close()
-            sleep(int(random() * (MAX_RANDOM_DELAY - MIN_RANDOM_DELAY)) + MIN_RANDOM_DELAY)
+            sleep(int(random() * (self.max_random_delay - self.min_random_delay)) + self.min_random_delay)
         sleep(20)
         print('bye!')
         self.driver.close()
@@ -79,12 +89,11 @@ class Bot:
         print('Opening post...')
         self.driver.get(f'https://www.instagram.com/p/{post_link}/')
 
-
     def load_followers(self) -> List[str]:
         if  not os.path.exists('followers.txt'):
             with open('followers.txt', 'wt') as file:
                 try:
-                    for count, follower in enumerate(self.scrape_followers(), 1):
+                    for count, follower in enumerate(self.scrape_followers(account=self.username), 1):
                         print("\t{:>3}: {}".format(count, follower))
                         file.write(follower+'\n')
                 except:
@@ -92,15 +101,15 @@ class Bot:
         return open('followers.txt','rt').readlines()
 
     def login(self) -> None:
-        self.driver.find_element_by_css_selector("input[name='username']").send_keys(USERNAME)
-        self.driver.find_element_by_css_selector("input[name='password']").send_keys(PASSWORD)
+        self.driver.find_element_by_css_selector("input[name='username']").send_keys(self.username)
+        self.driver.find_element_by_css_selector("input[name='password']").send_keys(self.password)
         self.driver.find_element_by_xpath("//button[@type='submit']").click()
         sleep(6)
 
     # I found here https://www.codegrepper.com/code-examples/whatever/scraping+instagram+followers+list+python
-    def scrape_followers(self) -> Iterator[str]:
+    def scrape_followers(self, account) -> Iterator[str]:
         # Load account page
-        self.driver.get("https://www.instagram.com/{0}/".format(self.username))
+        self.driver.get("https://www.instagram.com/{0}/".format(account))
 
         # Click the 'Follower(s)' link
         # driver.find_element_by_partial_link_text("follower").click
@@ -136,6 +145,18 @@ class Bot:
 
 
 if __name__ == "__main__":
+    if settings['username'] is None:
+        print('USERNAME is empty')
+        exit(0)
+    elif settings['password'] is None:
+        print('PASSWORD is empty')
+        exit(0)
+    elif settings['post_link'] is None:
+        print('LINK is empty')
+        exit(0)
     
-    bot = Bot(USERNAME,PASSWORD)
+    bot = Bot(username=settings['username'],password=settings['password'], post_link=settings['post_link'],
+              geckodriver=settings['geckodriver'],firefox_path=settings['firefox_path'],
+                headless=settings['headless'],custom_comment=settings['custom_comment'],
+                min_random_delay=settings['min_random_delay'],max_random_delay=settings['max_random_delay'])
     bot.start()
---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+settings={}
+settings['username']  = os.getenv('USERNAME')
+settings['password']  = os.getenv('PASSWORD')
+settings['post_link']  = os.getenv('LINK')
+settings['geckodriver']  = os.getenv('GECKODRIVER',False)
+settings['firefox_path']  = os.getenv('FIREFOX_PATH',False)
+settings['headless']  = os.getenv('HEADLESS',False)
+settings['custom_comment']  = os.getenv('CUSTOM_COMMENT',False)
+settings['min_random_delay']  = int(os.getenv('MIN_RANDOM_DELAY', 60))
+settings['max_random_delay']  = int(os.getenv('MAX_RANDOM_DELAY', 120))
+
